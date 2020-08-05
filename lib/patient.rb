250c252aa80831e21a949c378231225c078ee5a8
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-05 03:16:57 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,8 +14,8 @@ class Patient
     Appointment.new(date, self, doctor)
   end   
   
-  def appointments(patient)
-    
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
   end 
  
 end 
